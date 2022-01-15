c162044eb481490a31fe24a5c789a935022ca34b
kozhyarr <kozhyarr.rauf@gmail.com>
2022-01-15 23:31:04 +0300

    exercise4

---
+++ lib/screens/edit/bar.dart
@@ -0,0 +1,29 @@
+// TODO Complete the file, edit/bar.dart
+//-----------------------------------------------------------------------------------------------------------------------------
+//? Things to do:
+//   1. Build this screen with conditional UI technique:
+//        a. The title bar will show accordingly: 'Edit a todo' or 'Add a new todo'
+//-----------------------------------------------------------------------------------------------------------------------------
+
+import 'package:flutter/material.dart';
+import 'edit_screen.dart';
+
+class Bar extends StatelessWidget implements PreferredSizeWidget {
+  const Bar({state}) : _state = state;
+
+  final EditScreenState _state;
+  @override
+  Size get preferredSize => Size.fromHeight(50.0);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      // a. The title bar will show accordingly: 'Edit a todo' or 'Add a new todo'
+      title: _state.isEditing == true
+          ? Text('Edit a todo')
+          : Text('Add a new todo'),
+      leading: Container(),
+      centerTitle: true,
+    );
+  }
+}
